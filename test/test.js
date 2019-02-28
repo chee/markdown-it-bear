12e2d7cba3bbd3a60527fea74008d82df06d2f93
chee <chee@snoot.club>
2019-02-28 09:28:19 +0000

    Initial commit (polar bear)

--- test/test.js
+++ test/test.js
@@ -1,14 +1,8 @@
-'use strict';
+var path = require("path")
+var generate = require("markdown-it-testgen")
 
-
-var path     = require('path');
-var generate = require('markdown-it-testgen');
-
-/*eslint-env mocha*/
-
-describe('markdown-it-mark', function () {
-  var md = require('markdown-it')()
-              .use(require('../'));
-
-  generate(path.join(__dirname, 'fixtures/mark.txt'), md);
+describe("markdown-it-mark", function () {
+	var md = require("markdown-it")()
+	.use(require("../"))
+	generate(path.join(__dirname, "fixtures/mark.txt"), md)
 });
