778c5acb0aa514b506476c64cebb1c27225acd80
chee <chee@snoot.club>
2019-08-14 20:54:42 +0100

    yeet

---
+++ readme.md
@@ -0,0 +1,1 @@
+Any further development on markdown-it-bear will happen on https://git.snoot.club/chee/markdown-it-bear
--- test/test.js
+++
@@ -1,8 +0,0 @@
-var path = require("path")
-var generate = require("markdown-it-testgen")
-
-describe("markdown-it-polar-bear", function () {
-	var md = require("markdown-it")("zero")
-		.use(require("../"))
-	generate(path.join(__dirname, "fixtures/bear.txt"), md)
-});
